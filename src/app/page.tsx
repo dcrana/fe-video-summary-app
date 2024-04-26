4b84b01af78faccd74b1d3fcd2f1d07ec2e6eb5c
dcrana <divyarajsinhrana2487@gmail.com>
2024-04-26 12:06:20 +0530

    first fetch home page data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
-import { Button } from '@/components/ui/button'
+const getStrapiData = async (url: string) => {
+  const baseUrl = 'http://127.0.0.1:1337'
+  try {
+    const response = await fetch(baseUrl + url)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error(error)
+  }
+}
 
-const Home = () => {
+const Home = async () => {
+  const strapiData = await getStrapiData('/api/home-page')
+
+  const { title, description } = strapiData.data.attributes
+
   return (
-    <>
-      <Button>Shadcn button</Button>
-    </>
+    <main className="container mx-auto py-6">
+      <h1 className="text-5xl font-bold">{title}</h1>
+      <p className="text-xl mt-4">{description}</p>
+    </main>
   )
 }
+
 export default Home
